82d6fddecd7c918a9f85e332bceaa686c50ec087
ftheirs <fernando@zondax.ch>
2022-12-12 16:17:07 -0300

    remove deps source code

--- deps/nanosplus-secure-sdk/include/bolos_version.h
+++
@@ -1,4 +0,0 @@
-#ifndef BOLOS_VERSION
-#define BOLOS_VERSION "1.0.3"
-#endif /* BOLOS_VERSION */
-
--- deps/nanox-secure-sdk/include/bolos_version.h
+++
@@ -1,4 +0,0 @@
-#ifndef BOLOS_VERSION
-#define BOLOS_VERSION "2.0.2"
-#endif /* BOLOS_VERSION */
-
